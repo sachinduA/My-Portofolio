d77568fa0ec0a2434c9cc6cd9ce9de4af549811a
Sachindu Peramuna <sachinduayodye@gmail.com>
2022-02-17 17:36:32 +0530

    update projectsData

--- website/src/projectsData.js
+++ website/src/projectsData.js
@@ -2,20 +2,22 @@ const pdata = [
   {
     id: 1,
     title: "My Portofolio",
-    discription: "Created my portofolio using React js",
+    discription:
+      "Created At sat Feb 12 2022 14:13:57 GMT+05:30 (India Standard Time)",
     link: "https://github.com/sachinduA/My-Portofolio",
   },
   {
     id: 2,
     title: "Sample Java Project",
     discription:
-      "Web based systems allow you to control and monitor operations of your business from anywhere in the world.",
+      "Created At sat Feb 12 2022 14:13:57 GMT+05:30 (India Standard Time)",
     link: "https://github.com/sachinduA/Practical-Test",
   },
   {
     id: 3,
     title: "Research Management Project",
-    discription: "I am open for collaborate on your projects.",
+    discription:
+      "Simple Research Management application using java. Created At sat Feb 12 2022 14:13:57 GMT+05:30 (India Standard Time)",
     link: "https://github.com/sachinduA/PAF-group-project",
   },
 ];
